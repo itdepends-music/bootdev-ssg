93ab84b7161350b6915194fcee513a18e6041954
Oscar Robertson <itdependsmusic@proton.me>
2026-02-17 21:14:08 +1300

    add text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,28 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.IMAGE, "Link")
         node2 = TextNode("This is a text node", TextType.LINK, "Link")
         self.assertNotEqual(node, node2)
+
+
+class TestTextNodeToHTML_NODE(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.REGULAR)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertEqual(html_node.props, {"href": "https://www.google.com"})
+
+    def test_image(self):
+        node = TextNode("cat picture", TextType.IMAGE, "/cat.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props, {"src": "/cat.png", "alt": "cat picture"})
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     REGULAR = 1
@@ -25,3 +27,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({repr(self.text)}, {self.text_type}, {repr(self.url)})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.REGULAR:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})

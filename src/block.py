accdfa351d886b86cfeeea21d56fcb8e0b4e8b7d
Oscar Robertson <itdependsmusic@proton.me>
2026-02-20 23:11:52 +1300

    add function markdown_to_html_node

--- src/block.py
+++ src/block.py
@@ -1,6 +1,10 @@
 import re
 from enum import Enum
 
+from htmlnode import LeafNode, ParentNode
+from text_to_textnodes import text_to_textnodes
+from textnode import text_node_to_html_node
+
 
 class BlockType(Enum):
     PARAGRAPH = 1
@@ -33,3 +37,40 @@ def block_to_block_type(block_text):
     ):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+    def text_to_children(text):
+        text_nodes = text_to_textnodes(text)
+        return [text_node_to_html_node(node) for node in text_nodes]
+
+    html_nodes = []
+    for block in markdown_to_blocks(markdown):
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.PARAGRAPH:
+                html_nodes.append(
+                    ParentNode("p", text_to_children(block.replace("\n", " ")))
+                )
+            case BlockType.HEADING:
+                numHeading = 0
+                while block[numHeading] == "#":
+                    numHeading += 1
+                html_nodes.append(ParentNode(f"h{numHeading}", text_to_children(block)))
+            case BlockType.CODE:
+                text = block[4:-3]  # remove "```\n"
+                html_nodes.append(ParentNode("pre", [LeafNode("code", text)]))
+            case BlockType.QUOTE:
+                html_nodes.append(ParentNode("blockquote", text_to_children(block)))
+            case BlockType.UNORDERED_LIST:
+                items_text = [line[2:] for line in block]  # remove "- "
+                items_nodes = [LeafNode("li", text) for text in items_text]
+                html_nodes.append(ParentNode("ul", items_nodes))
+            case BlockType.ORDERED_LIST:
+                items_text = [
+                    line.split(".", maxsplit=1)[1][1:] for line in block
+                ]  # remove "{num}. "
+                items_nodes = [LeafNode("li", text) for text in items_text]
+                html_nodes.append(ParentNode("ol", items_nodes))
+
+    return ParentNode("div", html_nodes)
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,11 @@
 import unittest
 
-from block import BlockType, block_to_block_type, markdown_to_blocks
+from block import (
+    BlockType,
+    block_to_block_type,
+    markdown_to_blocks,
+    markdown_to_html_node,
+)
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -49,6 +54,40 @@ class TestBlockToBlockType(unittest.TestCase):
     def test_ordered_list(self):
         text = "1. This is an ordered list\n2. with items"
         self.assertEqual(block_to_block_type(text), BlockType.ORDERED_LIST)
+
+
+class TestMarkdownToHTMLNode(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
 
 
 if __name__ == "__main__":

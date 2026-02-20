1d81875f615a5f4e8b33ff6dd085132517301afc
Oscar Robertson <itdependsmusic@proton.me>
2026-02-20 15:00:03 +1300

    Add function block_to_block_type

--- src/block.py
+++ src/block.py
@@ -1,5 +1,35 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = 1
+    HEADING = 2
+    CODE = 3
+    QUOTE = 4
+    UNORDERED_LIST = 5
+    ORDERED_LIST = 6
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     blocks = (block.strip() for block in blocks)
     blocks = filter(lambda block: block != "", blocks)
     return list(blocks)
+
+
+def block_to_block_type(block_text):
+    if re.match(r"#{1,6}", block_text) is not None:
+        return BlockType.HEADING
+    if block_text.startswith("```\n") and block_text.endswith("```"):
+        return BlockType.CODE
+    if all(line.startswith(">") for line in block_text.split("\n")):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in block_text.split("\n")):
+        return BlockType.UNORDERED_LIST
+    if all(
+        line.startswith(f"{line_num + 1}. ")
+        for (line_num, line) in enumerate(block_text.split("\n"))
+    ):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -23,6 +23,32 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_paragraph(self):
+        text = "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+
+    def test_heading(self):
+        text = "## This is a heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+
+    def test_code(self):
+        text = '```\nprint("Hello World!")\n```'
+        self.assertEqual(block_to_block_type(text), BlockType.CODE)
+
+    def test_quote(self):
+        text = ">This is a quote\n>line 2"
+        self.assertEqual(block_to_block_type(text), BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        text = "- This is a list\n- with items"
+        self.assertEqual(block_to_block_type(text), BlockType.UNORDERED_LIST)
+
+    def test_ordered_list(self):
+        text = "1. This is an ordered list\n2. with items"
+        self.assertEqual(block_to_block_type(text), BlockType.ORDERED_LIST)
 
 
 if __name__ == "__main__":
